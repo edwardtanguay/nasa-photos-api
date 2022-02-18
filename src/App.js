213d1258eac9d59b37796bd492866c289a8ef6e7
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-02-18 11:07:21 +0100

    showing photo smaller

--- src/App.js
+++ src/App.js
@@ -2,11 +2,45 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.scss';
 
+const nasaUrl = 'https://epic.gsfc.nasa.gov/api/natural/date/2022-02-14';
+
 function App() {
-	const [photos, setPhotos] = useState([]);
+	const [photoObjects, setPhotoObjects] = useState([]);
+
+	useEffect(() => {
+		// IIFE - https://developer.mozilla.org/en-US/docs/Glossary/IIFE
+		(async () => {
+			try {
+				const response = await axios.get(nasaUrl);
+				const data = response.data;
+				const imageName = data['0'].image + '.jpg';
+				const caption = data['0'].caption;
+
+				setPhotoObjects([
+					{
+						imageName,
+						caption
+					}
+				]);
+			}
+			catch (err) {
+				console.error(err);
+			}
+		})();
+	}, []);
+
 	return (
 		<div className="App">
 			<h1>NASA Images</h1>
+			<ul className="photos">
+				{photoObjects.map((obj, i) => {
+					return (
+						<li key={i}>{obj.caption}
+							<img src={`https://epic.gsfc.nasa.gov/archive/natural/2022/02/14/jpg/${obj.imageName}`} alt="" />
+						</li>
+					)
+				})}
+			</ul>
 		</div>
 	);
 }
